bddc3f119d0e929b094b575b75c5f3ebc7867c97
Meeken <78071836@qq.com>
2020-02-23 15:51:10 +0800

    fixed

--- components/article/ArticleContent.jsx
+++ components/article/ArticleContent.jsx
@@ -128,9 +128,9 @@ class ArticleContent extends React.Component {
     translate(translateArr, {
       tld: "cn",
       to: "en",
-      //browers: true,
-      host: "127.0.0.1",
-      port: 9000
+      browers: true
+      // host: "127.0.0.1",
+      // port: 9000
     }).then(res => {
       const data = res.data[0]
       translateRes = parseMultiple(data)
