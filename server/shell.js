a966c5e4635c41d7229a0f34ea7a83e9545a3e5b
Meeken <78071836@qq.com>
2020-02-26 09:09:18 +0800

    fixed

--- server/shell.js
+++ server/shell.js
@@ -1,21 +1,31 @@
 const process = require("child_process")
 
-const _exec = function(shell) {
+const _exec = function(shell, isHidden = false) {
   return new Promise((resolve, reject) => {
     process.exec(shell, (error, stdout, stderr) => {
       if (error) {
         reject(error)
       } else {
+        if (!isHidden) console.log(stdout)
         resolve(stdout)
       }
     })
   })
 }
 
-_exec("git init && git add . && git commit -m 'fixed' && git push")
-  .then(res => {
-    console.log(res)
-  })
-  .catch(err => {
-    console.log(err)
-  })
+// _exec("git init && git add . && git commit -m 'fixed' && git push")
+//   .then(res => {
+//     console.log(res)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+
+const main = async function() {
+  await _exec("git init")
+  await _exec("git add .")
+  await _exec("git commit -m 'fixed'")
+  await _exec("git push gitee master")
+}
+
+main()
