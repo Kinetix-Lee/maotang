f8d355fb90068c392671e1592e7c084db7b75ec4
Meeken <78071836@qq.com>
2020-02-26 08:58:28 +0800

    fixed

--- server/shell.js
+++ server/shell.js
@@ -1,13 +1,24 @@
 const process = require("child_process")
 
-process.exec(
-  "cat ./public/static/config/maotang.json && git add . && git commit -m 'fixed' && git push",
-  (error, stdout, stderr) => {
-    if (error) {
-      console.error(`执行出错: ${error}`)
-      return
-    }
-    console.log(`${stdout}`)
-    console.log(`${stderr}`)
-  }
-)
+const _exec = function(shell) {
+  return new Promise((resolve, reject) => {
+    process.exec(shell, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`错误： ${error}`)
+        reject(error)
+      }
+      if (!error) {
+        console.log(`成功：${stdout}`)
+        resolve(stdout)
+      }
+      console.log(`错误：${stderr}`)
+      reject(error)
+    })
+  })
+}
+
+_exec(
+  "cat ./public/static/config/maotang.json && git add . && git commit -m 'fixed' && git push"
+).then(res => {
+  console.log(res)
+})
