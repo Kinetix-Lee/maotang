f5d5d1839668e5484b3b3cb58aff9aba585be49e
Meeken <42825670+Meeken1998@users.noreply.github.com>
2020-04-14 00:22:16 +0800

    fix: 删除无用代码

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const marked = require("marked")
 
 marked.setOptions({
   renderer: new marked.Renderer(),
-  // highlight(code) {
-  //   return require("highlight.js").highlightAuto(code).value
-  // },
   pedantic: false,
   gfm: true,
   tables: true,
